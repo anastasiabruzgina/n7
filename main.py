6cb729ce87fe7cfb55c1e0e80f09eeb4c79a71b1
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-11-14 13:41:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-def rot(num):
-    a = int(str(num).replace('6', '9', 1))
-    return a
+# -*- coding: utf-8 -*-
+def merge(*arrs):
+    arr_sum = []
+    for i in arrs:
+        arr_sum += i
+    
+    if len(arr_sum) < 2:
+        return arr_sum
+    
+    else:
+        const = arr_sum[0]
+        less = [i for i in arr_sum[1:] if i <= const]
+        greater = [i for i in arr_sum[1:] if i > const]
+        
+    return merge(less) + [const] + merge(greater)
+"""
+Created on Sat Nov 13 05:22:42 2021
 
-if __name__ == '__main__':
-    num = int(input())
-    print(rot(num))
+@author: anast
+"""
+
 
