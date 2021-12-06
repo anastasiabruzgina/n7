0a48c86ba8c043f974b4f09c9d2701a2f8a52218
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2021-12-06 20:06:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ def merge(*arrs):
         greater = [i for i in arr_sum[1:] if i > const]
         
     return merge(less) + [const] + merge(greater)
+
+def sort_sentence(sentence):
+    import operator
+    a = sentence.split(' ')
+    unsorted = {i : len(i) for i in a}
+    sorted_dict = sorted(unsorted.items(), key=operator.itemgetter(1))
+    result = ''
+    for key, i in sorted_dict:
+        result += key + ' '
+    return result
 """
 Created on Sat Nov 13 05:22:42 2021
 
